25bbca7ea5e522f3e7ba9c64da5d7d9330533255
Khushv4 <khushalvyas21@gmail.com>
2025-02-16 04:27:41 +0530

    started working with context api

---
+++ mini Context/src/components/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState, useContext } from 'react'
+import UserContext from '../../context/UserContext'
+
+function Profile() {
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
