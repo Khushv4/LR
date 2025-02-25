b764f320ee8fadab2612441006840dcd659e2e4d
Khushv4 <khushalvyas21@gmail.com>
2025-02-25 16:07:10 +0530

    configured store

--- WordFlow/src/appwrite/auth.js
+++ WordFlow/src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import conf from "../conf.js"
+import conf from "../conf/conf";
 import { Client, Account, ID } from "appwrite";
 
 export class AuthService {
@@ -53,6 +53,6 @@ export class AuthService {
 }
 const authService = new AuthService();
 
-export default AuthService;
+export default authService;
 
 
