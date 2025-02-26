7785a99b232e111e886c49cb38604387a2583edb
Khushv4 <khushalvyas21@gmail.com>
2025-02-27 02:05:38 +0530

    added other components

--- WordFlow/src/components/index.js
+++ WordFlow/src/components/index.js
@@ -3,6 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import LogoutBtm from "./Header/LogoutBtm";
 import Logo from "./Logo";
+ 
 
 export {
   Header,
